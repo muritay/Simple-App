eeb4d8607a5a8b41777a0386d94a5e431ede3e92
Babajide Yusuf <47525113+muritay@users.noreply.github.com>
2025-04-23 23:26:49 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='content'>
+      <h1>Hello World</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
